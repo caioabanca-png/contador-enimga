1c5ce3ba0e0a15b3916906a7a6bca3fdcc0e3aba
caioabanca-png <caioabanca@gmail.com>
2026-06-17 01:54:17 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+let gameState = {
+  videoPlayed: false,
+  timerStarted: false,
+  startTime: null,
+  solved: false
+};
+
+io.on("connection", (socket) => {
+  // Envia o estado atual para quem acabou de conectar
+  socket.emit("updateState", gameState);
+
+  socket.on("startVideo", () => {
+    gameState.videoPlayed = true;
+    io.emit("playVideo");
+  });
+
+  socket.on("startTimer", () => {
+    if (!gameState.timerStarted) {
+      gameState.timerStarted = true;
+      gameState.startTime = Date.now();
+      io.emit("startTimer", gameState.startTime);
+    }
+  });
+
+  socket.on("tryPassword", (pwd) => {
+    if (pwd === "142528") {
+      gameState.solved = true;
+      io.emit("accessGranted");
+    } else {
+      io.emit("accessDenied");
+    }
+  });
+});
+
+http.listen(process.env.PORT, () => {
+  console.log("Servidor rodando!");
+});
